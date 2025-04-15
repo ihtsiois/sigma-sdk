eaaf0871e4bcc4c4eb2452b821c63a635eacc95b
Achilles Gusmão <achilles@dazzledesign.com.br>
2025-04-15 19:33:22 -0300

    Update User Client Types

--- src/modules/users/types.ts
+++ src/modules/users/types.ts
@@ -1,40 +1,77 @@
-export type UserGender = 'MALE' | 'FEMALE' | 'NON_BINARY' | 'AGENDER' | 'GENDERQUEER' | 'TWO_SPIRIT' | 'OTHER';
-export type UserMaritalStatus = 'SINGLE' | 'MARRIED' | 'DIVORCED' | 'WIDOWED' | 'SEPARATED' | 'PARTNERED' | 'OTHER';
-export type UserEthnicity = 'WHITE' | 'BLACK' | 'BROWN' | 'YELLOW' | 'INDIGENOUS' | 'OTHER';
-export type UserContactType = 'EMAIL' | 'PHONE' | 'WEBSITE';
-
+export type UserID = string;
+export type UserActive = boolean;
+export type UserInternalEmail = string;
+export type UserFirstName = string;
+export type UserLastName = string | null;
+export type UserAvatarURL = string | null;
+export type UserLegalName = string;
+export type UserSocialName = string | null;
+export type UserGuardianName = string | null;
+export type UserBRCPF = string | null;
+export type userPassport = string | null;
+export type UserGender = 'MALE' | 'FEMALE' | 'NON_BINARY' | 'AGENDER' | 'GENDERQUEER' | 'TWO_SPIRIT' | 'OTHER' | null;
+export type UserNationality = string | null;
+export type UserMaritalStatus =
+    | 'SINGLE'
+    | 'MARRIED'
+    | 'DIVORCED'
+    | 'WIDOWED'
+    | 'SEPARATED'
+    | 'PARTNERED'
+    | 'OTHER'
+    | null;
+export type UserBirthDate = Date | null;
+export type UserEthnicity = 'WHITE' | 'BLACK' | 'BROWN' | 'YELLOW' | 'INDIGENOUS' | 'OTHER' | null;
+export type UserAddressCode = string | null;
+export type UserAddressCountry = string | null;
+export type UserAddressRegion = string | null;
+export type UserAddressCity = string | null;
+export type UserAddressNeighborhood = string | null;
+export type UserAddressStreet = string | null;
+export type UserAddressNumber = string | null;
+export type UserCreatedAt = Date;
+export type UserUpdatedAt = Date;
 export type ReadOnlyUserKeys = 'id' | 'active' | 'created_at' | 'updated_at' | 'contact_info';
 
-export type User = {
+export type UserContactID = string;
+export type UserContactType = 'EMAIL' | 'PHONE' | 'WEBSITE';
+export type UserContactValue = string;
+export type UserContactCreatedAt = Date;
+export type UserContactUpdatedAt = Date;
+
+export type PublicUserFirstName = UserFirstName;
+export type PublicUserAvatarURL = UserAvatarURL;
+
+export type UserResponse = {
     id: string;
     active: boolean;
     internal_email: string;
     first_name: string;
-    last_name?: string;
-    avatar_url?: string;
+    last_name: string | null;
+    avatar_url: string | null;
     legal_name: string;
-    social_name?: string;
-    guardian_name?: string;
-    br_cpf?: string;
-    passport?: string;
-    gender?: UserGender;
-    nationality?: string;
-    marital_status?: UserMaritalStatus;
-    birth_date?: Date;
-    ethnicity?: UserEthnicity;
-    contact_info: UserContact[];
-    address_code?: string;
-    address_country?: string;
-    address_region?: string;
-    address_city?: string;
-    address_neighborhood?: string;
-    address_street?: string;
-    address_number?: string;
-    created_at: Date;
-    updated_at: Date;
+    social_name: string | null;
+    guardian_name: string | null;
+    br_cpf: string | null;
+    passport: string | null;
+    gender: UserGender;
+    nationality: string | null;
+    marital_status: UserMaritalStatus;
+    birth_date: string | null;
+    ethnicity: UserEthnicity;
+    address_code: string | null;
+    address_country: string | null;
+    address_region: string | null;
+    address_city: string | null;
+    address_neighborhood: string | null;
+    address_street: string | null;
+    address_number: string | null;
+    contact_info: UserContactInfoResponse[];
+    created_at: string;
+    updated_at: string;
 };
 
-export type UserContact = {
+export type UserContactInfoResponse = {
     id: string;
     type: UserContactType;
     value: string;
@@ -42,10 +79,65 @@ export type UserContact = {
     updated_at: string;
 };
 
-export type PublicUser = {
-    first_name: string;
-    avatar_url: string | null;
+export type User = {
+    id: UserID;
+    active: UserActive;
+    internal_email: UserInternalEmail;
+    first_name: UserFirstName;
+    last_name: UserLastName;
+    avatar_url: UserAvatarURL;
+    legal_name: UserLegalName;
+    social_name: UserSocialName;
+    guardian_name: UserGuardianName;
+    br_cpf: UserBRCPF;
+    passport: userPassport;
+    gender: UserGender;
+    nationality: UserNationality;
+    marital_status: UserMaritalStatus;
+    birth_date: UserBirthDate;
+    ethnicity: UserEthnicity;
+    contact_info: UserContact[];
+    address_code: UserAddressCode;
+    address_country: UserAddressCountry;
+    address_region: UserAddressRegion;
+    address_city: UserAddressCity;
+    address_neighborhood: UserAddressNeighborhood;
+    address_street: UserAddressStreet;
+    address_number: UserAddressNumber;
+    created_at: UserCreatedAt;
+    updated_at: UserUpdatedAt;
 };
 
-export type CreateUserBody = Omit<User, ReadOnlyUserKeys | 'avatar_url'>;
-export type UpdateUserBody = Omit<Partial<User>, ReadOnlyUserKeys | 'internal_email' | 'avatar_url'>;
+export type UserContact = {
+    id: UserContactID;
+    type: UserContactType;
+    value: UserContactValue;
+    created_at: UserContactCreatedAt;
+    updated_at: UserContactUpdatedAt;
+};
+
+export type PublicUser = {
+    first_name: PublicUserFirstName;
+    avatar_url: PublicUserAvatarURL;
+};
+
+export type UserCreateBody = Omit<User, ReadOnlyUserKeys | 'avatar_url'>;
+export type UserCreateResponse = { data: UserResponse };
+export type UserCreateReturn = User;
+
+export type UserListResponse = { data: UserResponse[] };
+export type UserListReturn = User[];
+
+export type UserGetResponse = { data: UserResponse };
+export type UserGetReturn = User;
+
+export type UserUpdateBody = Omit<Partial<User>, ReadOnlyUserKeys | 'internal_email' | 'avatar_url'>;
+export type UserUpdateResponse = { data: UserResponse };
+export type UserUpdateReturn = User;
+
+export type UserDeleteReturn = void;
+
+export type UserUpdateAvatarBody = File;
+export type UserUpdateAvatarReturn = void;
+
+export type UserRemoveAvatarReturn = void;
--- src/modules/users/users-client.ts
+++ src/modules/users/users-client.ts
@@ -1,44 +1,51 @@
 import { HttpClient } from '../../client';
-import { CreateUserBody, UpdateUserBody, User } from './types';
+import * as types from './types';
 
 class UsersClient {
     constructor(private http: HttpClient) {}
 
-    private parse(body: any) {
+    private parse(body: types.UserResponse): types.User {
         return {
             ...body,
             birth_date: body.birth_date ? new Date(body.birth_date) : null,
             created_at: new Date(body.created_at),
             updated_at: new Date(body.updated_at),
-        } as User;
+            contact_info: body.contact_info.map((ci) => {
+                return {
+                    ...ci,
+                    created_at: new Date(ci.created_at),
+                    updated_at: new Date(ci.updated_at),
+                };
+            }),
+        };
     }
 
-    async create(body: CreateUserBody): Promise<User> {
-        const response = await this.http.post('/v1/users', body);
+    async create(body: types.UserCreateBody): Promise<types.UserCreateReturn> {
+        const response = await this.http.post<types.UserCreateResponse>('/v1/users', body);
         return this.parse(response.data);
     }
 
-    async list(): Promise<User[]> {
-        const response = await this.http.get(`/v1/users`);
+    async list(): Promise<types.UserListReturn> {
+        const response = await this.http.get<types.UserListResponse>(`/v1/users`);
         return response.data.map((user: any) => this.parse(user));
     }
 
-    async get(id: string): Promise<User> {
-        const response = await this.http.get(`/v1/users/${id}`);
+    async get(id: types.UserID): Promise<types.UserGetReturn> {
+        const response = await this.http.get<types.UserGetResponse>(`/v1/users/${id}`);
         return this.parse(response.data);
     }
 
-    async update(id: string, body: UpdateUserBody): Promise<User> {
-        const response = await this.http.put(`/v1/users/${id}`, body);
+    async update(id: types.UserID, body: types.UserUpdateBody): Promise<types.UserUpdateReturn> {
+        const response = await this.http.put<types.UserUpdateResponse>(`/v1/users/${id}`, body);
         return this.parse(response.data);
     }
 
-    async delete(id: string): Promise<void> {
+    async delete(id: types.UserID): Promise<types.UserDeleteReturn> {
         await this.http.delete(`/v1/users/${id}`);
         return;
     }
 
-    async updateAvatar(id: string, body: File): Promise<void> {
+    async updateAvatar(id: types.UserID, body: types.UserUpdateAvatarBody): Promise<types.UserUpdateAvatarReturn> {
         const formData = new FormData();
         formData.append('avatar', body);
 
@@ -46,7 +53,7 @@ class UsersClient {
         return;
     }
 
-    async removeAvatar(id: string): Promise<void> {
+    async removeAvatar(id: types.UserID): Promise<types.UserRemoveAvatarReturn> {
         await this.http.delete(`/v1/users/${id}/avatar`);
         return;
     }
